466caf2caf0f3d87fd8d72d8b90255fcedb2ed66
Amirabbas Asadi <amir137825@gmail.com>
2022-11-23 13:56:16 +0330

    adding a test for biased linear layer

--- tests/linear.cc
+++ tests/linear.cc
@@ -34,4 +34,36 @@ TEST_CASE("Linear Layer (double precision, no bias)", "[benchmark]") {
 
     delete[] X_in;
     delete[] X_layer;
+    delete[] X_out;
+}
+
+TEST_CASE("Linear Layer (double precision, biased)", "[benchmark]") {
+    using namespace rocky;
+    const unsigned N_in = 256;
+    const unsigned B_in = 2048;
+    const unsigned N_out = 128;
+
+    layer::linear<double, N_in, N_out> l1; 
+    REQUIRE ( l1.num_parameters() == (N_in+1)*N_out );
+    std::random_device rd;
+    std::mt19937 rnd_gen(rd());
+    std::uniform_real_distribution<double> dist(-1.0, 1.0);
+    auto sampler = std::bind(dist, rnd_gen);
+        
+    double* X_in = new double[B_in * N_in];
+    double* X_layer = new double[l1.num_parameters()];
+    double* X_out = new double[B_in * N_out];
+
+    std::generate(X_in, X_in + B_in * N_in, sampler);
+    std::generate(X_layer, X_layer + l1.num_parameters(), sampler);
+    
+    BENCHMARK("Forward Pass") {
+        l1.apply(X_layer, X_in, B_in, X_out);     
+        return;
+    };
+    
+
+    delete[] X_in;
+    delete[] X_layer;
+    delete[] X_out;
 }
