e2fa5481ed8d9cdd7ca841ff63ab9600a839938c
Amirabbas Asadi <amir137825@gmail.com>
2022-12-03 23:52:49 +0330

    logger class for Tribes

--- include/rocky/zagros/benchmark.h
+++ include/rocky/zagros/benchmark.h
@@ -3,13 +3,47 @@
 #define _USE_MATH_DEFINES
 
 #include<rocky/zagros/system.h>
+#include<string>
+#include<sstream>
 #include<cmath>
+#include<fstream>
 
 namespace rocky{
 namespace zagros
 {
 namespace benchmark
 {
+/**
+ * @brief PSO Log
+ * An object for logging the result of optimization
+ * The output will be saved as a .csv file
+ */
+template<typename T_e>
+class log_pso_tribes{
+    std::ofstream output;
+    int n_tribes;
+    int n_particles;
+
+public:
+    log_pso_tribes(std::string path, int n_particles, int n_tribes){
+        output.open(path, std::ofstream::out);
+        output << "time,tribe,value" << "\n";
+        this->n_particles = n_particles;
+        this->n_tribes = n_tribes;
+    }
+    void step(size_t time, const T_e* tribes_best_min_){
+        for(int t=0; t<n_tribes; t++){
+            output << time << "," << t << "," << tribes_best_min_[t] << "\n";
+        }
+    }
+    void save(){
+        output.close();
+    }
+    ~log_pso_tribes(){
+        if (output.is_open()) output.close();
+    }
+};
+
 
 /**
  * @brief Sphere function
@@ -23,6 +57,11 @@ public:
         for(size_t i=0; i<T_dim; i++)
             S += x[i] * x[i];
         return sqrt(S);
+    }
+    virtual std::string to_string(){
+        std::stringstream name;
+        name << "Sphere(dim=" << T_dim << ")";
+        return name.str();
     }
 };
 
@@ -38,6 +77,11 @@ public:
         for(size_t i=0; i<T_dim; i++)
             S += x[i] * x[i] - 10.0*cos(2*M_PI * x[i]);
         return S;
+    }
+    virtual std::string to_string(){
+        std::stringstream name;
+        name << "Rastrigin(dim=" << T_dim << ")";
+        return name.str();
     }
 };
 
