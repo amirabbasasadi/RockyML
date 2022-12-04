4e7ed537e1d1f71c6e8c49b84fb58f3470626640
Amirabbas Asadi <amir137825@gmail.com>
2022-12-04 18:45:26 +0330

    adding dimension as a template parameter

--- include/rocky/zagros/system.h
+++ include/rocky/zagros/system.h
@@ -1,12 +1,17 @@
 #ifndef ROCKY_ZAGROS_SYSTEM_GUARD
 #define ROCKY_ZAGROS_SYSTEM_GUARD
+#include<iostream>
+#include<string>
 namespace rocky{
 namespace zagros{
 
-template<typename T_e>
+template<typename T_e, size_t T_dim>
 class system{
 public:
     virtual T_e objective(T_e* params) = 0;
+    virtual std::string to_string(){
+        return "optimization problem";
+    }
 };
 
 }; // end of zagros namespace
