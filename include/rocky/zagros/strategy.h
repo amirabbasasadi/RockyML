8ea91f152039f76c274eece4e2e4c3922c80b357
Amirabbas Asadi <amir137825@gmail.com>
2022-12-16 12:29:11 +0330

    rewriting the procedure for finding best solution in each node

--- include/rocky/zagros/strategy.h
+++ include/rocky/zagros/strategy.h
@@ -158,9 +158,10 @@ class broadcast_best_solution: public mpi_strategy<T_e, T_dim>{
 template<typename T_e, int T_dim>
 class basic_pso: public search_strategy<T_e, T_dim>{
     typedef Eigen::Map<Eigen::Matrix<T_e, 1, T_dim, Eigen::RowMajor>> eigen_particle;
-    // typedef basic_scontainer<T_e, T_dim> container;
     
     enum phase {phase_I, phase_II, phase_III, phase_IV};
+    enum update_mode {use_particles, use_groups};
+
 protected:
     // system
     system<T_e, T_dim>* problem_;
@@ -195,24 +196,7 @@ public:
         this->groups_best_ = groups_best;
         this->node_best_ = node_best;
         this->cluster_best = cluster_best;
-
     }
- // allocate the required memory
-    // virtual void allocate(){
-    //     spdlog::info("allocating memory for particles history");
-    //     particles_best_ = std::make_unique<swarm>(main_swarm_->n_particles(), main_swarm_->group_size());
-    //     particles_best_->allocate();
-    //     // allocate particles memory
-    //     particles_v_.resize(main_swarm_->n_particles());
-    //     for(int p=0; p<main_swarm_->n_particles(); ++p)
-    //         particles_v_[p].resize(T_dim);
-    //      // allocate groups memory
-    //     groups_best_min_.resize(main_swarm_->n_groups());
-    //     groups_best_argmin_.resize(main_swarm_->n_groups());
-    //     std::fill(groups_best_min_.begin(), groups_best_min_.end(), std::numeric_limits<T_e>::max());
-    //     // best cluster solution
-    //     cluster_best_argmin_.resize(T_dim);
-    // }
     T_e rand_uniform(){
         static std::uniform_real_distribution<T_e> dist(0.0, 1.0);
         return dist(rocky::utils::random::prng);
@@ -262,6 +246,20 @@ public:
     virtual void update_groups_best(){
         update_groups_best(0, this->main_container_->n_groups());
     }
+    template<update_mode T_um=use_groups>
+    void update_node_best(){
+        if constexpr(T_um == use_groups){
+            auto min_el = std::min_element(this->groups_best_->values.begin(),
+                                           this->groups_best_->values.end());
+            if (*min_el < this->node_best_->values[0]){
+                int min_el_ind = static_cast<int>(min_el - this->groups_best_->values.begin());
+                std::copy(this->groups_best_->particle(min_el_ind),
+                          this->groups_best_->particle(min_el_ind)+T_dim,
+                          this->node_best_->particle(0));
+                node_best_->values[0] = *min_el;
+            }
+        }
+    }
     /**
      * @brief update particles velocity in parallel
      * 
@@ -291,11 +289,11 @@ public:
                                            + (2.0 * this->rand_uniform() * (p_best_gr - x));
                 }         
             }
-            // if constexpr(T_phase == phase::phase_III){
-            //     eigen_particle p_best_n(this->node_best_argmin_);
-            //      v = v * this->hyper_w_ + (2.0 * this->rand_uniform() * (p_best - x)) 
-            //                             + (2.0 * this->rand_uniform() * (p_best_n - x));
-            // }
+            if constexpr(T_phase == phase::phase_III){
+                eigen_particle p_best_n(this->node_best_->particle(0));
+                 v = v * this->hyper_w_ + (2.0 * this->rand_uniform() * (p_best - x)) 
+                                        + (2.0 * this->rand_uniform() * (p_best_n - x));
+            }
             // if constexpr(T_phase == phase::phase_IV){
             //     eigen_particle p_best_c(this->cluster_best_argmin_.data());
             //      v = v * this->hyper_w_ + (2.0 * this->rand_uniform() * (p_best - x)) 
@@ -319,6 +317,8 @@ public:
         this->hyper_w_ = rand_uniform();
         this->update_particles_best();
         this->update_groups_best();
+        this->update_node_best();
+        spdlog::info("best node solution is {}", this->node_best_->values[0]);
         this->update_particles_v<phase_I>();
         this->update_particles_x();
     }
