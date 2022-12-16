ce0987557d7cad6a677b0627db587606917afac3
Amirabbas Asadi <amir137825@gmail.com>
2022-12-17 01:49:45 +0330

    parallelizing mutations

--- include/rocky/zagros/strategies/genetic.h
+++ include/rocky/zagros/strategies/genetic.h
@@ -27,11 +27,11 @@ protected:
     int k_;
 public:
     T_e sample_dim(){
-        static std::uniform_int_distribution<> dist(0, T_dim);
+        std::uniform_int_distribution<> dist(0, T_dim-1);
         return dist(rocky::utils::random::prng);
     }
-    T_e sample_particle(){
-        static std::uniform_int_distribution<> dist(0, target_container_->n_particles()-1);
+    T_e sample_particle(int start_rng, int end_rng){
+        static std::uniform_int_distribution<> dist(start_rng, end_rng);
         return dist(rocky::utils::random::prng);
     }    
     dimension_tweak_strategy(system<T_e, T_dim>* problem, basic_scontainer<T_e, T_dim>* tgt_container, int k){
@@ -41,29 +41,34 @@ public:
     }
     virtual void tweak(int p_ind, int dim) = 0;
     virtual void apply(){
-        int dim, p_ind;
-        // sample a particle
-        p_ind = this->sample_particle();
-        // save a backup from affected dims
-        auto solution_backup_dims = std::make_unique<int[]>(k_);
-        auto solution_backup_vals = std::make_unique<T_e[]>(k_);
-        // apply mutation on k dimensions
-        for(int d=0; d<k_; d++){
-            // choose a random dim
-            dim = this->sample_dim();
-            solution_backup_dims[d] = dim;
-            solution_backup_vals[d] = target_container_->particles[p_ind][dim];
-            // apply the mutation on the dimension
-            this->tweak(p_ind, dim);
-        }
-        // check if the mutation caused any improvement
-        T_e obj_val = problem_->objective(target_container_->particle(p_ind));
-        if(obj_val < target_container_->values[p_ind])
-            target_container_->values[p_ind] = obj_val;
-        else
-            for(int d=0; d<k_; d++)
-                target_container_->particles[p_ind][solution_backup_dims[d]] = solution_backup_vals[d];
-                
+        int n_groups = this->target_container_->n_groups();
+        tbb::parallel_for(0, n_groups, 1, [this](int gr){
+            int dim, p_ind;
+            auto group_rng = this->target_container_->group_range(gr);
+            // sample a particle
+            p_ind = this->sample_particle(group_rng.first, group_rng.second-1);
+            // save a backup from affected dims
+            auto solution_backup_dims = std::make_unique<int[]>(k_);
+            auto solution_backup_vals = std::make_unique<T_e[]>(k_);
+            // apply mutation on k dimensions
+            for(int d=0; d<k_; d++){
+                // choose a random dim
+                dim = this->sample_dim();
+                solution_backup_dims[d] = dim;
+                solution_backup_vals[d] = target_container_->particles[p_ind][dim];
+                // apply the mutation on the dimension
+                this->tweak(p_ind, dim);
+            }
+            // check if the mutation caused any improvement
+            T_e obj_val = problem_->objective(target_container_->particle(p_ind));
+            if(obj_val < target_container_->values[p_ind])
+                target_container_->values[p_ind] = obj_val;
+            else
+                for(int d=0; d<k_; d++)
+                    target_container_->particles[p_ind][solution_backup_dims[d]] = solution_backup_vals[d];
+                    
+        });
+        
     }
 }; 
 
