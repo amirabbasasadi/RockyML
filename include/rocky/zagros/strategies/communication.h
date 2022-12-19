c268917051eb015afdb5bedee673fb55855e727d
Amirabbas Asadi <amir137825@gmail.com>
2022-12-19 19:00:59 +0330

    communication strategy based on RMA

--- include/rocky/zagros/strategies/communication.h
+++ include/rocky/zagros/strategies/communication.h
@@ -90,6 +90,122 @@ public:
         cluster_best_container_->values[0] = result.cluster_best_min;
     }
 };
+
+/**
+ * @brief A communication strategy for sharing best solution using RMA passive target model
+ * 
+ */
+template<typename T_e, int T_dim>
+class rma_share_best: public mpi_strategy<T_e, T_dim>{
+protected:
+    // MPI windows
+    MPI_Win argmin_window_;
+    MPI_Win min_window_;
+    MPI_Win lock_window_;
+
+    basic_scontainer<T_e, T_dim>* cluster_best_container_;
+    basic_scontainer<T_e, T_dim>* temp_container_;
+    basic_scontainer<T_e, T_dim>* window_cnt_;
+    /**
+     * @brief lock for local operations
+     * for global operations we use MPI_lock
+     */
+    std::vector<int> lock_; 
+public:
+    // allocate a window for sharing the solution on all processes
+    void allocate_window(){
+        // a window for the solution
+        MPI_Win_create(window_cnt_->particle(0), T_dim * sizeof(T_e), sizeof(T_e),
+                       MPI_INFO_NULL, MPI_COMM_WORLD, &argmin_window_); 
+        // a window for the value of the solution
+        MPI_Win_create(window_cnt_->value(0), sizeof(T_e), sizeof(T_e),
+                       MPI_INFO_NULL, MPI_COMM_WORLD, &min_window_); 
+        // a window for locking
+        MPI_Win_create(lock_.data(), sizeof(int), sizeof(int),
+                       MPI_INFO_NULL, MPI_COMM_WORLD, &lock_window_);   
+    }
+    // free the allocated memory
+    void free_window(){
+        MPI_Win_free(&argmin_window_);
+        MPI_Win_free(&min_window_);
+        MPI_Win_free(&lock_window_);
+    }
+    int random_process(int n_procs){
+        static std::uniform_int_distribution<> dist(0, n_procs-1);
+        return dist(rocky::utils::random::prng);
+    }
+    virtual void apply(){
+        // identify the process
+        int rank = this->mpi_rank();
+        int n_procs = this->mpi_num_procs();
+        // choose a random target process
+        int target_ranks = rank; 
+        while(target_ranks == rank)
+            target_ranks = this->random_process(n_procs);
+       spdlog::info("P({}) chooses P({}) as a target", rank, target_rank);
+        // check to see if the target process has a better solution
+        MPI_Get(temp_container_->value(0), 1, MPI_FLOAT, target_rank, 0, 1, MPI_FLOAT, min_window);
+        
+        spdg::info("P({}) gets min value from P({}) : ", rank, target_rank, temp_container_->values[0]);
+        if(temp_container_->values[0] < cluster_best_container_->values[0]){
+            int locked = 1;
+            // notify the target process
+            MPI_Put(&locked, 1, MPI_INT, target_rank, rank, 1, MPI_INT, lock_window_);
+            // get the solution
+            MPI_Get(cluster_best_container_->value(0), 1, MPI_FLOAT, target_rank, 0, T_dim, MPI_FLOAT, argmin_window);
+            locked = 0;
+            // notify the target process
+            MPI_Put(&locked, 1, MPI_INT, target_rank, rank, 1, MPI_INT, lock_window_);
+        }
+
+    }
+};
+
+
+
+/**
+ * @brief An async communication strategy for broadcasting best solution
+ * 
+ */
+template<typename T_e, int T_dim>
+class async_broadcast_best: public mpi_strategy<T_e, T_dim>{
+protected:
+    // handlers to ckeck the status of message
+    MPI_Request reduction_req_;
+    MPI_Request broadcast_req_;
+
+    basic_scontainer<T_e, T_dim>* cluster_best_container_;
+public:
+    async_broadcast_best(basic_scontainer<T_e, T_dim>* container){
+        this->cluster_best_container_ = container;
+        this->fetch_mpi_info();
+    }
+    virtual void apply(){
+        // identify the process
+        int rank = this->mpi_rank();
+        // ask everyone in the cluster to find the min value
+        struct {
+            T_e cluster_best_min;
+            int rank;
+        } data_out, result;
+
+        data_out.cluster_best_min = cluster_best_container_->values[0];
+        data_out.rank = rank;
+
+        if constexpr(std::is_same<T_e, double>::value){
+            MPI_Iallreduce(&data_out, &result, 1, MPI_DOUBLE_INT, MPI_MINLOC, MPI_COMM_WORLD, &reduction_req_);
+            MPI_Ibcast(cluster_best_container_->particle(0), T_dim, MPI_DOUBLE, result.rank, MPI_COMM_WORLD, &broadcast_req_);
+        }
+        if constexpr(std::is_same<T_e, float>::value){
+            MPI_Iallreduce(&data_out, &result, 1, MPI_FLOAT_INT, MPI_MINLOC, MPI_COMM_WORLD, &reduction_req_);
+            spdlog::info("reduction is done. the min process is {} and min value is {}", result.rank, result.cluster_best_min);
+            MPI_Ibcast(cluster_best_container_->particle(0), T_dim, MPI_FLOAT, result.rank, MPI_COMM_WORLD, &broadcast_req_);
+        } 
+        cluster_best_container_->values[0] = result.cluster_best_min;
+    }
+};
+
+
 #endif
 
 };
