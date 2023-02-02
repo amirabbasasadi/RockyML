7fb3d95912ad0796dcf84a2e4b9331f63c905cf9
Amirabbas Asadi <amir137825@gmail.com>
2023-02-02 20:50:02 +0330

    prepare auto benchmark for testing performance

--- benchmarks/benchmark_rastrigin/main.cc
+++ benchmarks/benchmark_rastrigin/main.cc
@@ -1,43 +1,54 @@
 #define ROCKY_USE_MPI
 #include <rocky/zagros/benchmark.h>
 #include <rocky/zagros/flow.h>
+#include <rocky/zagros/strategies/eda.h>
 
+#include<chrono>
+
+using namespace rocky;
+using namespace zagros::dena;
+using namespace std::chrono;
+    
+flow get_optimizer(int n_particles){
+    return container::create("A", n_particles)
+              >> init::uniform("A") 
+              >> run::n_times(500,
+                        mutate::gaussian("A")
+                        >> run::with_probability(0.2,
+                            crossover::differential_evolution("A")
+                        )
+              );
+}
 
 int main(int argc, char* argv[]){
-    using namespace rocky;
     MPI_Init(&argc, &argv);
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
-    using namespace rocky;
+    typedef float solution_type;
 
-    typedef float swarm_type;
-
-    const int n_particles = 300;
-    const int group_size = 10;
-    const int dim = 2000;
+    const int dim = 100;
     const int block_dim = 100;
 
-    using namespace zagros::dena;
+    zagros::benchmark::rastrigin<solution_type> problem(dim);
+
+    auto n_threads = argv[1];
+
+    std::fstream fh("../benchmark.data", std::fstream::app);
+
+    std::vector<int> particles {100, 200, 400, 600, 800, 1000, 2000, 4000, 6000, 8000, 10000};
     
+    for(int i=0; i<particles.size(); i++){
+        auto f = get_optimizer(particles[i]);
+        auto start = high_resolution_clock::now();
+        zagros::basic_runtime<solution_type, dim, block_dim> runtime(&problem);
+        runtime.run(f);
+        auto end = high_resolution_clock::now();
+        auto duration = duration_cast<milliseconds>(end - start);
+        fh << fmt::format("{},{},{}", n_threads, particles[i], duration.count()) << std::endl;
+    }
 
-    zagros::benchmark::rastrigin<swarm_type> problem(dim);
-    std::fstream f;
-    zagros::local_optimization_log log_handler(f, "loss.csv");
-
-    auto f2 = container::create("A", n_particles, group_size)
-              >> pso::memory::create("M", "A")
-              >> init::uniform("A")
-              >> run::n_times(60,
-                    blocked_descent::uniform::step()
-                    >> run::n_times(20, 
-                            pso::group::step("M", "A"))
-                    >> run::n_times(60, 
-                            pso::cluster::step("M", "A")
-                            >> run::with_probability(0.2, log::local::best(log_handler))));
-
-    zagros::basic_runtime<swarm_type, dim, block_dim> runtime(&problem);
-    runtime.run(f2);
+    fh.close();
+ 
     MPI_Finalize();
     return 0;
 }
--- benchmarks/generate.sh
+++ benchmarks/generate.sh
@@ -2,17 +2,21 @@
 
 # number of threads
 n_threads=4
-n_threads_per_process=4
 # number of MPI processes
 n_procs=1
 
 for benchmark_dir in benchmark_* ; do
+    rm -rf "${benchmark_dir}/build" && 
+    mkdir "${benchmark_dir}/build" &&
     (cd "${benchmark_dir}/build" &&
     cmake .. -DCMAKE_CXX_COMPILER=mpic++ -DCMAKE_TOOLCHAIN_FILE=../../../../../vcpkg/scripts/buildsystems/vcpkg.cmake &&
     make -j${n_threads} app &&
-    mpirun --use-hwthread-cpus -np ${n_procs} --map-by node:PE=${n_threads_per_process} ./app &&
+    (for ((n_threads_per_process = 1 ; n_threads_per_process <= $n_threads ; n_threads_per_process=n_threads_per_process*2)); do
+        mpirun --use-hwthread-cpus -np ${n_procs} --map-by node:PE=${n_threads_per_process} ./app ${n_threads_per_process}
+    done) && 
     mkdir -p ../result &&
-    lscpu > ../result/cpu_info.txt &&
-    for p in `seq 0 $(expr $n_procs - 1)`; do cp "proc_${p}_loss.csv" ../result/; done &&
-    python3 ../../../tools/plot_logs.py proc_0_loss.csv)
+    lscpu > ../result/cpu_info.txt
+    # for p in `seq 0 $(expr $n_procs - 1)`; do cp "proc_${p}_loss.csv" ../result/; done &&
+    # python3 ../../../tools/plot_logs.py proc_0_loss.csv
+    )
 done
