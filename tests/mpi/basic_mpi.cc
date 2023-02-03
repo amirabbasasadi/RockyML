254bfbda380952db74d4ba5087cb3a69cbfe8e28
Amirabbas Asadi <amir137825@gmail.com>
2023-02-03 17:41:06 +0330

    updating test according to recent changes

--- tests/mpi/basic_mpi.cc
+++ tests/mpi/basic_mpi.cc
@@ -1,48 +1,44 @@
 #define ROCKY_USE_MPI
 #include <rocky/zagros/benchmark.h>
 #include <rocky/zagros/flow.h>
-#include <rocky/zagros/strategies/eda.h>
 
-
+using namespace rocky;
+using namespace zagros::dena;
+    
 int main(int argc, char* argv[]){
-    using namespace rocky;
     MPI_Init(&argc, &argv);
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
-    using namespace rocky;
 
-    typedef float swarm_type;
+    typedef float solution_type;
 
     const int n_particles = 300;
-    const int group_size = 10;
-    const int dim = 200;
-    const int block_dim = 20;
+    const int group_size = 20;
+    const int dim = 1000;
+    const int block_dim = 100;
 
-    using namespace zagros::dena;
+    zagros::benchmark::rastrigin<solution_type> problem(dim);
     
+    zagros::local_log_handler log_handler("best_solution.csv");
 
-    zagros::benchmark::rastrigin<swarm_type> problem(dim);
-    std::fstream f;
-    zagros::local_optimization_log log_handler(f, "loss.csv");
-    
-    auto f2 = container::create("A", n_particles, group_size)
+    auto f1 = container::create("A", n_particles, group_size)
               >> pso::memory::create("M", "A")
               >> init::uniform("A")
-              >> run::while_improve(10,
-                    blocked_descent::uniform::step()
-                    >> run::while_improve(5, 
+              >> run::while_improve(300,
+                    block::uniform::select()
+                    >> run::while_improve(10, 
                             pso::local::step("M", "A")
-                            >> mutate::gaussian(pso::memory::particles_mem("M"), 4)
-                            >> crossover::multipoint(pso::memory::particles_mem("M"))
+                            >> mutate::gaussian(pso::memory::particles_mem("M"))
                             >> run::with_probability(0.05, log::local::best(log_handler)))
-                    >> run::while_improve(5, 
+                    >> run::while_improve(10, 
                             pso::global::step("M", "A")
+                            >> run::with_probability(0.4, crossover::differential_evolution(pso::memory::particles_mem("M")))
                             >> run::with_probability(0.05, log::local::best(log_handler))));
 
-    zagros::basic_runtime<swarm_type, dim, block_dim> runtime(&problem);
-    
-    runtime.run(f2);
+    zagros::basic_runtime<solution_type, dim, block_dim> runtime(&problem);
+ 
+    runtime.run(f1);
+
     MPI_Finalize();
     return 0;
 }
